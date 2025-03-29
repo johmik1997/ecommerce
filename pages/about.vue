9c311d959d3bfb97153a26b10730d10b379a037f
yohannes <yohannegeta9@gmail.com>
2025-03-28 19:44:58 -0600

    add favicon

--- pages/about.vue
+++ pages/about.vue
@@ -63,6 +63,7 @@ nav ul li a.active {
     max-width: 800px;
     margin: 20px auto;
     padding: 20px;
+    color: black;
     background-color: #f9f9f9;
     border-radius: 10px;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
